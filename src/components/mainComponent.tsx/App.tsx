8c74bac5e77cd502f3c72c6d3f37a7b0a2fa6e8d
julian mercado <gustavo.n.mercado2@gmail.com>
2021-06-27 20:50:55 -0300

    styled component added

--- src/components/mainComponent.tsx/App.tsx
+++ src/components/mainComponent.tsx/App.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import './App.scss';
-
+import styled from "styled-components"
 function App() {
+  const MyH1 = styled.h1`
+    font-size: 34px;
+    font-weight: 800;
+    color: #ff0000;
+  `
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyH1> Welcome to a new app </MyH1>
     </div>
   );
 }
